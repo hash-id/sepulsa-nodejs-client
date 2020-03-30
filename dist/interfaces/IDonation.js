b66d88273f2a0ce877953b8c254989edeb838a4a
M. Akmalul 'Ulya <secret9092@gmail.com>
2020-03-30 15:21:04 +0700

    donation dist files

---
+++ dist/interfaces/IDonation.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var DonationSandboxEnum;
+(function (DonationSandboxEnum) {
+    DonationSandboxEnum["sbox-081234000001"] = "081234000001";
+    DonationSandboxEnum["sbox-081234000004"] = "081234000004";
+})(DonationSandboxEnum = exports.DonationSandboxEnum || (exports.DonationSandboxEnum = {}));
